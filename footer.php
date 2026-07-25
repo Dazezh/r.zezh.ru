a65ab551e322d2aa05a47bc2c3e44bee24fb57f3
RB <lord@l1m1n.ru>
2026-07-25 19:34:04 +0300

    add readme

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 </main>
 <footer class="site-footer">
     <div class="shell footer-grid">
-        <div><strong>ZEZH</strong>
+        <div>
+            <a class="text-link" href="https://github.com/Dazezh/r.zezh.ru">Исходный код на GitHub</a>
             <p>Веб, сервисы и проекты без корпоративного тумана.</p>
         </div>
         <div><?php wp_nav_menu(['theme_location' => 'footer', 'container' => false, 'fallback_cb' => false]); ?></div>
